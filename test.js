8ce76dd0a8431fcc1d11040339c497443989dcf9
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2018-01-28 23:03:56 +0200

    Add: basic support to load shallow modules sync and call sync functions

--- test.js
+++ test.js
@@ -12,61 +12,60 @@ jail.setSync('_ivm', ivm);
 jail.setSync('_log', new ivm.Reference(function(...args) {
   console.log(...args);
 }));
-jail.setSync('_dir', new ivm.Reference(function(...args) {
-  console.dir(...args);
-}));
 
-jail.setSync('_require', new ivm.Reference(function(packageName) {
-
-  const pack = require(packageName);
-
-  // transform all fns in references
-
-  const makeRefs = (object) => {
-    // if it's a function, ref it
-    if (typeof object === 'function') {
-      return new ivm.Reference(object);
-      // return new ivm.Reference(function(...args) {
-      //   return object(...args);
-      //   const data = object(...args);
-      //   return new ivm.ExternalCopy(data).copyInto();
-      // });
+const packModuleFunctions = (module) => {
+  return packModuleFunctions.pack(module);
+};
+packModuleFunctions.pack = (value) => {
+  if (typeof value === 'function') {
+    return {
+      type: 'function',
+      value: undefined
     }
-
-    // if it's an array map on it and cycle this
-    if (Array.isArray(object)) {
-      return object.map(x => makeRefs(x));
-    }
-
-    // if it's an object map on its kets and cycle this
-    if (typeof object === 'object' && object !== null) {
-      const keys = Object.keys(object);
-      const newObj = {};
-      for (let i = 0; i < keys.length; i++) {
-        const key = keys[i];
-
-        newObj[key] = makeRefs(object[key]);
-      }
-      return newObj;
-    }
-
-    // if it's a primitive just pass it
-    // might fail on promises though
-    return object;
-  };
-
-  const repacked = makeRefs(pack);
-
-  let out;
-  if (repacked instanceof ivm.Reference) {
-    out = repacked;
-  } else {
-    out = new ivm.Reference(repacked);
   }
-  return out;
+
+  if (Array.isArray(value)) {
+    return {
+      type: 'array',
+      value: value.map((x, i) => packModuleFunctions.pack(x))
+    };
+  }
+
+  if (typeof value === 'object' && value !== null) {
+    const keys = Object.keys(value);
+    const newObj = {};
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      newObj[key] = packModuleFunctions.pack(value[key]);
+    }
+    return {
+      type: 'object',
+      value: newObj
+    };
+  }
+
+  return {
+    type: 'primitive',
+    value: value
+  };
+};
+
+jail.setSync('__require_packed_module', new ivm.Reference(function(moduleName) {
+  return new ivm.ExternalCopy(packModuleFunctions(require(moduleName))).copyInto();
+}));
+jail.setSync('__apply_module_function', new ivm.Reference(function(moduleName, trace, args) {
+
+  let pointer = require(moduleName);
+  trace.forEach(link => pointer = pointer[link]);
+
+  const result = pointer(...args);
+
+  return new ivm.ExternalCopy(result).copyInto();
 }));
 
-const bootstrap = isolate.compileScriptSync('(' + (function() {
+// bootstrap
+isolate.compileScriptSync('(' + (() => {
+
   let ivm = _ivm;
   delete _ivm;
 
@@ -76,76 +75,69 @@ const bootstrap = isolate.compileScriptSync('(' + (function() {
     log.applySync(undefined, args.map(arg => new ivm.ExternalCopy(arg).copyInto()));
   };
 
-  let dir = _dir;
-  delete _dir;
-  global.dir = function(...args) {
-    dir.applySync(undefined, args.map(arg => new ivm.ExternalCopy(arg).copyInto()));
+
+  let _require_packed_module = __require_packed_module;
+  let _apply_module_function = __apply_module_function;
+  delete __require_packed_module;
+  delete __apply_module_function;
+
+  global.requireNative = (moduleName) => {
+    const packedModule = _require_packed_module.applySync(undefined, [moduleName]);
+    global.requireNative._bindPackedModule(moduleName, packedModule, []);
+    return global.requireNative._unpackPackedModule(packedModule);
   };
-
-  let require = _require;
-  delete _require;
-
-  const fixFnsInRequire = global.fixFnsInRequire = (object) => {
-
-    // this might conflict with a genuine module that contains typeof as a string === 'function'
-    if (object.typeof === 'function') {
-      return (...args) => {
-        global.log('will apply on', object, args);
-        global.log('apply?', typeof object.apply, 'applySync?', typeof object.applySync);
-        return object.applySync(undefined, args.map(arg => new ivm.ExternalCopy(arg).copyInto()));
+  global.requireNative._bindPackedModule = (moduleName, pack, trace) => {
+    if (pack.type === 'function') {
+      pack.value = (...args) => {
+        return _apply_module_function.applySync(
+          undefined,
+          [
+            moduleName, trace, args
+          ].map(x => new ivm.ExternalCopy(x).copyInto())
+        );
       }
+    } else if (pack.type === 'array') {
+      pack
+        .value
+        .forEach((packet, index) =>
+          global.requireNative._bindPackedModule(moduleName, packet, trace.concat(index))
+        );
+    } else if (pack.type === 'object') {
+      const keys = Object.keys(pack.value);
+      keys.forEach(key => {
+        const packet = pack.value[key];
+        global.requireNative._bindPackedModule(moduleName, packet, trace.concat(key));
+      });
+    }
+  };
+  global.requireNative._unpackPackedModule = (pack) => {
+    if (pack.type === 'array') {
+      return pack.value.map(packet => global.requireNative._unpackPackedModule(packet));
+    }
+    if (pack.type === 'object') {
+      const keys = Object.keys(pack.value);
+      const newObj = {};
+      keys.forEach(key => {
+        const packet = pack.value[key];
+        newObj[key] = global.requireNative._unpackPackedModule(packet);
+      });
+      return newObj;
     }
 
-    if (Array.isArray(object)) {
-      return object.map(x => fixFnsInRequire(x));
-    }
-
-    if (typeof object === 'object' && object !== null) {
-      const keys = Object.keys(object);
-      const newObject = {};
-      for (let i = 0; i < keys.length; i++) {
-        const key = keys[i];
-        newObject[key] = fixFnsInRequire(object[key]);
-      }
-      return newObject;
-    }
-
-    return object;
+    // for functions and primitives we just pass through
+    return pack.value;
   };
 
-  global.requireNative = function(packageName) {
-    if (global.requireNative.cache.has(packageName)) {
-      global.log('returned cached', packageName);
-      return global.requireNative.cache.get(packageName);
-    }
-
-    const pack = require.applySync(undefined, [packageName]);
-    global.log('required', packageName);
-    const unpacked = pack;fixFnsInRequire(pack);
-
-    global.requireNative.cache.set(packageName, unpacked);
-    return unpacked;
-  };
-  global.requireNative.cache = new Map();
-
-}) + ')()');
-
-bootstrap.runSync(context);
-
-isolate.compileScriptSync('(' + (async () => {
-
-  // const uuid = global.requireNative('uuid');
-  const fs = global.requireNative('fs');
-
-  // global.log('uuid:', uuid());
-  // global.log('uuid:', global.requireNative('uuid')());
-
-  global.dir(fs.deref());
-  // global.dir(Object.keys(fs));
-  // global.dir(fs.existsSync('./'));
-  // global.log(fs.readFileSync('./package.json'));
-
-}) + ')();').runSync(context);
+}) + ')()').runSync(context);
 
 
-// setInterval(() => {}, 100);
+// test
+isolate.compileScriptSync('(' + (() => {
+
+  const _package = requireNative('fs');
+
+  log('fs is', _package);
+  log('fs.existsSync', typeof _package.existsSync);
+  log('fs.existsSync(".")', _package.existsSync('.'));
+
+}) + ')()').runSync(context);
